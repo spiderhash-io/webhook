22eb26c1c014b250001e681b548191c5ae11fae8
Eduards Marhelis Twino <142914080+emarhelis@users.noreply.github.com>
2023-11-17 20:33:20 +0200

    add ci

--- src/config.py
+++ src/config.py
@@ -1,72 +1,16 @@
+import json
+
 from src.modules.rabbitmq import RabbitMQConnectionPool
 
 # data type [ "text", "json", "blob", "form" ]
 # processing modules [ "rmq","kafka","redis","save_to_disk", "save_to_db", "websocket" ]
 # authorization [ "basic", "secret", "JWT", "HMAC" ]
 
-# Mock of the configuration data structure
-webhook_config_data = {
-    'abcde': {
-        'data_type': 'json',
-        'module': 'save_to_disk',
-        'module-config': {
-            "path": "webhooks"
-        },
-        'authorization': 'secret'
-    },
-    'toredis': {
-        'data_type': 'json',
-        'module': 'redis_rq',
-        'connection': 'redis-local'
-    },
-    'torabbit': {
-        'data_type': 'json',
-        'module': 'rabbitmq',
-        'connection': 'rabbitmq-local',
-        'queue_name': 'webhooks'
-    },
-    'torabbit2': {
-        'data_type': 'json',
-        'module': 'rabbitmq',
-        'connection': 'rabbitmq-local2',
-        'queue_name': 'webhooks2'
-    },
-    'workflow': {
-        'data_type': 'json',
-        'module': 'temporalio',
-        'connection': 'rabbitmq-local2',
-        'queue_name': 'webhooks2'
-    }
-}
+with open("webhooks.json", 'r') as webhooks_file:
+    webhook_config_data = json.load(webhooks_file)
 
-# connection config
-connection_config = {
-    # 'redis-local': {
-    #     'type': 'redis-rq',
-    #     'host': '0.0.0.0',
-    #     'port': '6379',
-    #     'db': '0',
-    #     'queue_name': 'beta',
-    # },
-    'rabbitmq-local': {
-        'type': 'rabbitmq',
-        'host': '0.0.0.0',
-        'port': '5672',
-        'user': 'guest',
-        'pass': 'guest',
-        
-    },
-    'rabbitmq-local2': {
-        'type': 'rabbitmq',
-        'host': '0.0.0.0',
-        'port': '5672',
-        'user': 'guest',
-        'pass': 'guest',
-        
-    },
-}
-
-
+with open("connections.json", 'r') as connections_file:
+    connection_config = json.load(connections_file)
 
 
 async def inject_connection_details(webhook_config_data, connection_config):
